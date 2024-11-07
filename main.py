baea495f7e27ef2f715a8850bcdc523addde4bcb
Alessfan <99832183+Alessfan@users.noreply.github.com>
2024-11-07 15:02:26 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Expense Tracker Application Starter Code
+
+def main():
+    print("Welcome to the Expense Tracker")
+    # Implement menu and functionality here
+
+if __name__ == "__main__":
+    main()
